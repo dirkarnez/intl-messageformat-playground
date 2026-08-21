77ed068c4ace9986f1d4f79a5b65b6e5ce624fac
Dirk Arnez <smalldirkalex@gmail.com>
2026-08-21 18:27:33 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[minimal-react-development](https://dirkarnez.github.io/minimal-react-development)
+==================================================================================
+Not for production
+
+### TODOs
+- [x] Lazy-loaded
---
+++ loaded-component.js
@@ -0,0 +1,13 @@
+/* 
+"module.exports" / "export default" is not needed.
+browser version of babel do all the work for us.
+*/
+
+function LoadedComponent() {
+	return (
+	  <div className="myDiv">
+		<h2>This is a heading in a div element</h2>
+		<p>This is some text in a div element.</p>
+	  </div>
+	)
+}
